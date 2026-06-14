c7fbad3928f3f239bc5a096f4ada478cfdf40078
batman <aakashrohtela02@gmail.com>
2026-06-14 01:01:30 -0400

    tailwind css installation and setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Task Manager Managment</div>
+    <div className='text-red-500 text-3xl underline'>Task Manager Managment</div>
   )
 }
 
