c5cc60dd9668ab298ac6ad17424829cba204d6d7
batman <aakashrohtela02@gmail.com>
2026-06-17 20:41:15 -0400

    upload image api route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express"
-import { signin, signup, updateUserProfile, userProfile } from "../controller/auth.controller.js"
+import { signin, signup, updateUserProfile, uploadImage, userProfile } from "../controller/auth.controller.js"
 import { verifyToken } from "../utils/VerifyUser.js" 
+import upload from "../utils/multer.js"
 
 const router=express.Router()
 router.post("/signup",signup)
@@ -8,4 +9,5 @@ router.post("/signin",signin);
 
 router.get("/userProfile",verifyToken,userProfile)
 router.put("/update-profile",verifyToken,updateUserProfile)
+router.post("/upload-image",upload.single("image"),uploadImage)
 export default router;
